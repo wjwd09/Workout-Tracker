0e73a485006a3187ffb1e2c71c6d1a11945e35f5
Walker <48071189+wjwd09@users.noreply.github.com>
2023-09-07 12:21:44 -0400

    Created rough model of exercise format

---
+++ models/liftNumbersModel.js
@@ -0,0 +1,22 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const liftNumbersSchema = new Schema({
+    Exercise: {
+        type: String,
+        required: true
+    },
+    Sets: {
+        type: String
+    },
+    Reps: {
+        type: String
+    },
+    Weight: {
+        type: String,
+        default: 0,
+        required: true
+    }
+});
+
+module.exports = mongoose.model("liftNumbersModel", liftNumbersSchema);
