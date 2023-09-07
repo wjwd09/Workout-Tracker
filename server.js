ec4a49c6e88d5cfaba527e2514b8b5b00131dba6
Walker <48071189+wjwd09@users.noreply.github.com>
2023-09-07 12:20:57 -0400

    Simplified API setup and created working DB connection

--- server.js
+++ server.js
@@ -1,23 +1,26 @@
+require('dotenv').config()
+
 const express = require("express");
-const liftNumbers = require("./liftNumbers.js");
 const app = express();
 const port = 3000;
-const connectToMongo = require("./db.js");
+const mongoose = require("mongoose");
+
+mongoose.connect(process.env.DB_URL);
+const db = mongoose.connection;
+db.on('error', (error) => console.error(error));
+db.once('open', () => console.log("Connected to DB"));
 
 app.use(express.json());
 
-app.get("/", function (req, res) {
-    console.log("GET request!");
-    res.send("Hello World!");
+const liftNumbersRouter = require('./routes/liftNumbers');
+app.use('/liftNumbers', liftNumbersRouter);
+
+app.listen(port, function () {
+  console.log(`Workout app listening on port ${port}!`);
 });
+
 
 module.exports = app;
 
-app.listen(port, function () {
-  console.log(`Example app listening on port ${port}!`);
-});
 
-connectToMongo();
-const blogRouter = require("./routes/BlogRoutes.js");
 
-app.use("/api/blogs", blogRouter)
